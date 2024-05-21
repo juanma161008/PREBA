c051cf7199775af8bdfa85fd3a110ff373e18260
CARLOS ANDRES DAVILA TORO <cadavila@correo.iue.edu.co>
2024-05-21 08:12:13 -0500

    creation of the back of the nomina

--- skyedge/formularios/frmnomina.cs
+++ skyedge/formularios/frmnomina.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using skyedge.clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,66 @@ namespace skyedge.formularios
 {
     public partial class frmnomina : Form
     {
+        Cconexion cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+        int contador, i, boton;
         public frmnomina()
         {
             InitializeComponent();
+            cn = new Cconexion();
+            i = 0; boton = 0;
+            cmd = new SqlCommand("select * from tblUsuario", cn.AbrirConexion());
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
         }
+        void llenar(DataTable dt, int i)
+        {
+            txtcedula.Text = dt.Rows[i][0].ToString();
+            txtnombre.Text = dt.Rows[i][1].ToString();
+
+            contador = dt.Rows.Count;
+        }
+        private void txtcedula_Leave(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("select * from tblUsuario where IdUsuario = '" + txtcedula.Text + "'",
+               cn.AbrirConexion());
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+
+
+            if (dt.Rows.Count > 0)
+            {
+                llenar(dt, i);
+            }
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            double salarioBaseHora = Convert.ToDouble(txtsalariobasico.Text);
+            double horasTrabajadas = Convert.ToDouble(txthoras.Text);
+            double salud = Convert.ToDouble(txtsalud.Text);
+            double pension = Convert.ToDouble(txtpension.Text);
+            double retefuente = Convert.ToDouble(txtretefuente.Text);
+            double auxilioTransporte = Convert.ToDouble(txtretefuente.Text);
+            double horasExtras = Convert.ToDouble(txthorasextras.Text);
+            double valorExtras = Convert.ToDouble(txtvalorextras.Text);
+
+            double salarioBruto = salarioBaseHora * horasTrabajadas;
+            double totalDevengado = salarioBruto + auxilioTransporte + (horasExtras * valorExtras);
+
+            totalDevengado -= salud + pension + retefuente;
+
+            txtdevengado.Text = totalDevengado.ToString("N2");
+        }
+
+      
+
+      
+       
+
     }
 }
--- skyedge/formularios/frmusuarionuevo.Designer.cs
+++ skyedge/formularios/frmusuarionuevo.Designer.cs
@@ -53,9 +53,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Sans Serif Collection", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(703, 43);
+            this.label1.Location = new System.Drawing.Point(527, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(640, 56);
+            this.label1.Size = new System.Drawing.Size(531, 47);
             this.label1.TabIndex = 0;
             this.label1.Text = "REGISTRO DE USUARIOS NUEVOS";
             // 
@@ -64,9 +65,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Sans Serif Collection", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(56, 16);
+            this.label2.Location = new System.Drawing.Point(42, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 37);
+            this.label2.Size = new System.Drawing.Size(91, 29);
             this.label2.TabIndex = 0;
             this.label2.Text = "NOMBRE";
             // 
@@ -75,9 +77,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(57, 220);
+            this.label3.Location = new System.Drawing.Point(43, 179);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 32);
+            this.label3.Size = new System.Drawing.Size(64, 27);
             this.label3.TabIndex = 1;
             this.label3.Text = "EMAIL";
             // 
@@ -86,9 +89,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(57, 266);
+            this.label4.Location = new System.Drawing.Point(43, 216);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(136, 32);
+            this.label4.Size = new System.Drawing.Size(117, 27);
             this.label4.TabIndex = 2;
             this.label4.Text = "PASSWORD";
             // 
@@ -97,9 +101,10 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(57, 71);
+            this.label5.Location = new System.Drawing.Point(43, 58);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(130, 32);
+            this.label5.Size = new System.Drawing.Size(111, 27);
             this.label5.TabIndex = 3;
             this.label5.Text = "APELLIDOS";
             // 
@@ -108,51 +113,53 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(57, 123);
+            this.label6.Location = new System.Drawing.Point(43, 100);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(127, 32);
+            this.label6.Size = new System.Drawing.Size(110, 27);
             this.label6.TabIndex = 4;
             this.label6.Text = "TELEFONO";
             // 
             // txtnombre
             // 
             this.txtnombre.BackColor = System.Drawing.Color.White;
-            this.txtnombre.Location = new System.Drawing.Point(205, 25);
-            this.txtnombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtnombre.Location = new System.Drawing.Point(154, 20);
+            this.txtnombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnombre.Name = "txtnombre";
-            this.txtnombre.Size = new System.Drawing.Size(209, 22);
+            this.txtnombre.Size = new System.Drawing.Size(158, 20);
             this.txtnombre.TabIndex = 5;
+            
             // 
             // txtapellidos
             // 
-            this.txtapellidos.Location = new System.Drawing.Point(205, 71);
-            this.txtapellidos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtapellidos.Location = new System.Drawing.Point(154, 58);
+            this.txtapellidos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtapellidos.Name = "txtapellidos";
-            this.txtapellidos.Size = new System.Drawing.Size(209, 22);
+            this.txtapellidos.Size = new System.Drawing.Size(158, 20);
             this.txtapellidos.TabIndex = 6;
             // 
             // txttelefono
             // 
-            this.txttelefono.Location = new System.Drawing.Point(205, 123);
-            this.txttelefono.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txttelefono.Location = new System.Drawing.Point(154, 100);
+            this.txttelefono.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txttelefono.Name = "txttelefono";
-            this.txttelefono.Size = new System.Drawing.Size(209, 22);
+            this.txttelefono.Size = new System.Drawing.Size(158, 20);
             this.txttelefono.TabIndex = 7;
             // 
             // txtemail
             // 
-            this.txtemail.Location = new System.Drawing.Point(205, 220);
-            this.txtemail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtemail.Location = new System.Drawing.Point(154, 179);
+            this.txtemail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtemail.Name = "txtemail";
-            this.txtemail.Size = new System.Drawing.Size(209, 22);
+            this.txtemail.Size = new System.Drawing.Size(158, 20);
             this.txtemail.TabIndex = 8;
             // 
             // txtpassword
             // 
-            this.txtpassword.Location = new System.Drawing.Point(205, 266);
-            this.txtpassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtpassword.Location = new System.Drawing.Point(154, 216);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(209, 22);
+            this.txtpassword.Size = new System.Drawing.Size(158, 20);
             this.txtpassword.TabIndex = 9;
             // 
             // panel1
@@ -171,10 +178,10 @@
             this.panel1.Controls.Add(this.txtapellidos);
             this.panel1.Controls.Add(this.txtnombre);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(431, 178);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Location = new System.Drawing.Point(323, 145);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(501, 375);
+            this.panel1.Size = new System.Drawing.Size(376, 305);
             this.panel1.TabIndex = 1;
             // 
             // label7
@@ -182,18 +189,19 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(59, 171);
+            this.label7.Location = new System.Drawing.Point(44, 139);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(99, 32);
+            this.label7.Size = new System.Drawing.Size(85, 27);
             this.label7.TabIndex = 11;
             this.label7.Text = "CEDULA";
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(207, 171);
-            this.txtCedula.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCedula.Location = new System.Drawing.Point(155, 139);
+            this.txtCedula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCedula.Name = "txtCedula";
-            this.txtCedula.Size = new System.Drawing.Size(209, 22);
+            this.txtCedula.Size = new System.Drawing.Size(158, 20);
             this.txtCedula.TabIndex = 12;
             this.txtCedula.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -201,10 +209,10 @@
             // 
             this.btnguardar.BackColor = System.Drawing.Color.SlateBlue;
             this.btnguardar.ForeColor = System.Drawing.Color.White;
-            this.btnguardar.Location = new System.Drawing.Point(191, 311);
-            this.btnguardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnguardar.Location = new System.Drawing.Point(143, 253);
+            this.btnguardar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnguardar.Name = "btnguardar";
-            this.btnguardar.Size = new System.Drawing.Size(139, 36);
+            this.btnguardar.Size = new System.Drawing.Size(104, 29);
             this.btnguardar.TabIndex = 10;
             this.btnguardar.Text = "GUARDAR";
             this.btnguardar.UseVisualStyleBackColor = false;
@@ -212,13 +220,13 @@
             // 
             // frmusuarionuevo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1444, 582);
+            this.ClientSize = new System.Drawing.Size(1028, 473);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmusuarionuevo";
             this.Text = "frmcliente";
             this.Load += new System.EventHandler(this.frmusuarionuevo_Load);
