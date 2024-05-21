342bca111b4032de5b924af5bf831267a567e29f
JUAN MANUEL CARDENAS MONTOYA <jmcardenas@correo.iue.edu.co>
2024-05-20 23:42:50 -0500

    UPLOAD

---
+++ skyedge/dbo.tblEmpleados.data.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [dbo].[tblEmpleados] ([Id_Personal], [Nombre], [Apellido], [Password], [Telefono], [email], [Direccion], [Cargo], [Salario])
+VALUES 
+(123456789, 'Juan', 'Pérez', '123', '123456789', 'juan@example.com', 'Calle Principal 123', 'Gerente', 2500.00),
+(987654321, 'María', 'Gómez', '123', '987654321', 'maria@example.com', 'Avenida Central 456', 'Asistente', 1800.00),
+(567890123, 'Carlos', 'Martínez', '123', '567890123', 'carlos@example.com', 'Carrera Secundaria 789', 'Analista', 2000.00);
