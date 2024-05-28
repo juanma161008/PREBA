d4d11d371c3a3fb70cdb4f7a153b7ad4a9741e6f
gojanwe <141775394+Gojanwe@users.noreply.github.com>
2024-05-28 07:39:39 -0500

    update

---
+++ skyedge/formularios/empleados/frmconsultaliquidacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace skyedge.formularios.empleados
+{
+    public partial class frmconsultaliquidacion : Form
+    {
+        public frmconsultaliquidacion()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
