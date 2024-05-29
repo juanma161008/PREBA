681dee30c80958d0d061b66841e65747ce101534
JUAN MANUEL CARDENAS MONTOYA <jmcardenas@correo.iue.edu.co>
2024-05-29 11:30:40 -0500

    update

--- skyedge/Program.cs
+++ skyedge/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using skyedge.formularios;
-using skyedge.formularios.empleados;
 
 
 namespace skyedge
@@ -19,7 +18,7 @@ namespace skyedge
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmconsultaliquidacion());
+            Application.Run(new frmnomina());
         }
     }
 }
