be6dd04a9f6767e7b36bf44ad67ed5c27117c6e2
DanielHenao001 <danielhenaocorrales759@gmail.com>
2024-05-30 06:21:55 -0500

    modificareserva

--- skyedge/Program.cs
+++ skyedge/Program.cs
@@ -20,7 +20,7 @@ namespace skyedge
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmnomina());
+            Application.Run(new frmmodificareserva());
         }
     }
 }
