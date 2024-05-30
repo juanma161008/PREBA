45307eca77ffad07f6a2abebbb94148e4c1d21b7
DanielHenao001 <danielhenaocorrales759@gmail.com>
2024-05-29 20:37:32 -0500

    update

---
+++ skyedge/formularios/empleados/frmmenuempleado.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace skyedge.formularios.empleados
+{
+    public partial class frmmenuempleado : Form
+    {
+        public frmmenuempleado()
+        {
+            InitializeComponent();
+        }
+
+        private void frmmenuempleado_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnconsulta_Click(object sender, EventArgs e)
+        {
+            frmconsultaliquidacion consultaLiquidacionForm = new frmconsultaliquidacion();
+            consultaLiquidacionForm.Show();
+
+        }
+    }
+}
---
+++ skyedge/formularios/empleados/frmreportes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace skyedge.formularios.empleados
+{
+    public partial class frmreportes : Form
+    {
+        public frmreportes()
+        {
+            InitializeComponent();
+        }
+
+        private void frmreportes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
